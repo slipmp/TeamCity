39b2f6d1f440c80fbf56d76363b00323bd5268bd
slipmp <slipmp@hotmail.com>
2014-07-05 22:59:56 -0400

    Adicionando testes unitários

---
+++ TeamCityFirstSolution/TeamCityWeb.Tests/TeamCityTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TeamCityWeb.Tests
+{
+    [TestClass]
+    public class TeamCityTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
